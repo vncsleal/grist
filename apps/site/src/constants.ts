c96461b7f54e833b089ed5038b178b2263af7bfd
Vinicius Leal <viniciusleal.s@gmail.com>
2026-06-10 16:38:10 -0300

    fix: restore macosDmg link (DMG pipeline is now working)

--- apps/site/src/constants.ts
+++ apps/site/src/constants.ts
@@ -2,6 +2,7 @@ const GITHUB_RELEASES = "https://github.com/vncsleal/quillby/releases/latest/dow
 
 export const DOWNLOAD_URLS = {
   macosPkg: `${GITHUB_RELEASES}/quillby-macos.pkg`,
+  macosDmg: `${GITHUB_RELEASES}/quillby-macos.dmg`,
   windowsExe: `${GITHUB_RELEASES}/quillby-windows.exe`,
   releasesPage: "https://github.com/vncsleal/quillby/releases",
 } as const;
