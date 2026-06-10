82700d20e6cde81425524b5ea24d6b0379fecaf2
Vinicius Leal <viniciusleal.s@gmail.com>
2026-06-10 16:36:41 -0300

    fix: remove dead macosDmg link from constants

--- apps/site/src/constants.ts
+++ apps/site/src/constants.ts
@@ -2,7 +2,6 @@ const GITHUB_RELEASES = "https://github.com/vncsleal/quillby/releases/latest/dow
 
 export const DOWNLOAD_URLS = {
   macosPkg: `${GITHUB_RELEASES}/quillby-macos.pkg`,
-  macosDmg: `${GITHUB_RELEASES}/quillby-macos.dmg`,
   windowsExe: `${GITHUB_RELEASES}/quillby-windows.exe`,
   releasesPage: "https://github.com/vncsleal/quillby/releases",
 } as const;
